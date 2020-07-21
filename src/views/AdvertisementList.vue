78caf1d25a99f5953b276e4accbffd71e1ab7991
Andrey <andrey.lipetsk99@gmail.com>
2020-07-21 19:53:07 +0300

    Added filtering and paging

--- src/views/AdvertisementList.vue
+++ src/views/AdvertisementList.vue
@@ -1,5 +1,35 @@
 <template>
         <div id="advertisements">
+            <div class="">
+                <div>
+                    <label>Искать в заголовке: </label>
+                    <input type="text" v-model="titleInputValue"/>
+                </div>
+                <div>
+                    <label>Искать в описании: </label>
+                    <input type="text" v-model="descriptionInputValue"/>
+                </div>
+                <div>
+                    <label>Размер страницы: </label>
+                    <select v-model="pageSizeSelector">
+                        <option>Все</option>
+                        <option>10</option>
+                        <option>20</option>
+                        <option>50</option>
+                        <option>100</option>
+                    </select>
+                </div>
+                <div>
+                    <button v-on:click="refreshList">Обновить</button>
+                </div>
+                <div v-if="isPaging === true" class="page-selector">
+                    <a v-on:click="toFirstPage">First</a>
+                    <a v-on:click="toPrevPage">Prev</a>
+                    <a v-for="i in totalPages" :key="i" v-on:click="toPage(i)">{{ i }}</a>
+                    <a v-on:click="toNextPage">Next</a>
+                    <a v-on:click="toLastPage">Last</a>
+                </div>
+            </div>
             <div>
                 <table border="1">
                     <tr>
@@ -14,7 +44,7 @@
                             <td><a :href="'http://localhost:3000/advertisements/' + adv.id">{{ adv.id }}</a></td>
                             <td>{{ adv.title }}</td>
                             <td>{{ adv.description }}</td>
-                            <td>{{ adv.addDateTime }}</td>
+                            <td>{{ adv.addDateTime | formatTimestamp }}</td>
                             <td>{{ adv.categoryId }}</td>
                         </tr>
                     </template>
@@ -33,12 +63,74 @@ export default {
                 advs: null,
                 titleInputValue: '',
                 descriptionInputValue: '',
-                pageSizeSelector: ''
+                pageSizeSelector: '',
+                pageNumber: 1,
+                totalPages: 1,
+                isPaging: false
             };
     },
     methods: {
+        refreshList: function() {
+            let isHasParam = false;
+            let request = 'http://localhost:8080/advertisement/list';
+
+            if (this.titleInputValue !== '') {
+                request += `${isHasParam === true ? '&' : '?'}title=${this.titleInputValue}`;
+                isHasParam = true;
+            }
+            if (this.descriptionInputValue !== '') {
+                request += `${isHasParam === true ? '&' : '?'}description=${this.descriptionInputValue}`;
+                isHasParam = true;
+            }
+            if (this.pageSizeSelector !== 'Все') {
+                request += `${isHasParam === true ? '&' : '?'}page_size=${this.pageSizeSelector}`;
+                request += `&page_number=${this.pageNumber}`;
+                isHasParam = true;
+            }
+            console.log(request);
+            axios.get(request).then(response => {
+                if (this.pageSizeSelector === 'Все') {
+                    this.advs = response.data;
+                    this.isPaging = false;
+                }
+                else {
+                    this.advs = response.data.content;
+                    this.totalPages = response.data.totalPages;
+                    this.isPaging = true;
+                }
+
+                console.log(this.totalPages);
+            });
+        },
+        toFirstPage: function() {
+            this.pageNumber = 1;
+            this.refreshList();
+        },
+        toPrevPage: function() {
+            this.pageNumber = this.pageNumber - 1;
+            if (this.pageNumber < 1) this.pageNumber = 1;
+            this.refreshList();
+        },
+        toPage: function(i) {
+            this.pageNumber = i;
+            this.refreshList();
+        },
+        toNextPage: function() {
+            this.pageNumber = this.pageNumber + 1;
+            if (this.pageNumber > this.totalPages) this.pageNumber = this.totalPages;
+            this.refreshList();
+        },
+        toLastPage: function() {
+            this.pageNumber = this.totalPages;
+            this.refreshList();
+        },
         getAdvertisements: function() {
             axios.get('http://localhost:8080/advertisement/list').then(response => (this.advs = response.data));
+        }
+    },
+    filters: {
+        formatTimestamp: function(value) {
+            return value.substring(0, 10) + " " + value.substring(11, 19);
         }
     },
     mounted: function() {
@@ -62,5 +154,12 @@ li {
 }
 a {
   color: #42b983;
+}
+.page-selector a {
+    margin: 0 0.25rem;
+}
+.page-selector a:hover {
+    text-decoration: underline;
+    cursor: pointer;
 }
 </style>
