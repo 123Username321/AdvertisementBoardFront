aca5a2923712dc56d498cfec9dca0810d22d1586
Andrey <andrey.lipetsk99@gmail.com>
2020-07-22 10:06:49 +0300

    Added new filters

--- src/views/AdvertisementList.vue
+++ src/views/AdvertisementList.vue
@@ -3,15 +3,34 @@
             <div class="">
                 <div>
                     <label>Искать в заголовке: </label>
-                    <input type="text" v-model="titleInputValue"/>
+                    <input type="text" v-model="filters.title"/>
                 </div>
                 <div>
                     <label>Искать в описании: </label>
-                    <input type="text" v-model="descriptionInputValue"/>
+                    <input type="text" v-model="filters.description"/>
+                </div>
+                <div>
+                    <label>Категория: </label>
+                    <select v-model="filters.category">
+                        <option v-for="elem in categories" :key="elem">{{ elem }}</option>
+                    </select>
+                </div>
+                <div>
+                    <label>От: </label>
+                    <input type="date" v-model="filters.startDate" />
+                    <button v-on:click="filters.startDate = null">Сбросить</button>
+                </div>
+                <div>
+                    <label>До: </label>
+                    <input type="date" v-model="filters.endDate" />
+                    <button v-on:click="filters.endDate = null">Сбросить</button>
+                </div>
+                <div>
+                    <button v-on:click="refreshList">Обновить</button>
                 </div>
                 <div>
                     <label>Размер страницы: </label>
-                    <select v-model="pageSizeSelector">
+                    <select v-model="pageSizeSelector" v-on:change="setPaging">
                         <option>Все</option>
                         <option>10</option>
                         <option>20</option>
@@ -19,15 +38,14 @@
                         <option>100</option>
                     </select>
                 </div>
-                <div>
-                    <button v-on:click="refreshList">Обновить</button>
-                </div>
                 <div v-if="isPaging === true" class="page-selector">
-                    <a v-on:click="toFirstPage">First</a>
-                    <a v-on:click="toPrevPage">Prev</a>
-                    <a v-for="i in totalPages" :key="i" v-on:click="toPage(i)">{{ i }}</a>
-                    <a v-on:click="toNextPage">Next</a>
-                    <a v-on:click="toLastPage">Last</a>
+                    <a v-on:click="toPage(1)">Первая</a>
+                    <a v-on:click="toPage(pageNumber - 1)">Предыдущая</a>
+                    <select v-on:change="toPage(pageNumber)" v-model="pageNumber">
+                        <option v-for="i in totalPages" :key="i">{{ i }}</option>
+                    </select>
+                    <a v-on:click="toPage(pageNumber + 1)">Следующая</a>
+                    <a v-on:click="toPage(totalPages)">Последняя</a>
                 </div>
             </div>
             <div>
@@ -45,7 +63,7 @@
                             <td>{{ adv.title }}</td>
                             <td>{{ adv.description }}</td>
                             <td>{{ adv.addDateTime | formatTimestamp }}</td>
-                            <td>{{ adv.categoryId }}</td>
+                            <td>{{ categories[adv.categoryId] }}</td>
                         </tr>
                     </template>
                 </table>
@@ -61,68 +79,94 @@ export default {
     data: function() {
             return { 
                 advs: null,
-                titleInputValue: '',
-                descriptionInputValue: '',
+                lastRequest: '',
                 pageSizeSelector: '',
                 pageNumber: 1,
                 totalPages: 1,
-                isPaging: false
+                isPaging: false,
+                inputs: {
+                    titleTag: '',
+                    descriptionTag: '',
+                    categoryTag: '',
+                    startDateTag: null,
+                    endDateTag: null
+                },
+                filters: {
+                    title: '',
+                    description: '',
+                    category: 'Все',
+                    startDate: null,
+                    endDate: null
+                },
+                lastFilters: null,
+                categories: [
+                    'Все',
+                    'Недвижимость',
+                    'Личный транспорт',
+                    'Бытовая техника',
+                    'Персональная электроника',
+                    'Одежда и обувь',
+                    'Спорт и отдых',
+                    'Прочее'
+                ]
             };
     },
     methods: {
-        refreshList: function() {
-            let isHasParam = false;
+        refreshList: function(isChangePage = false) {
             let request = 'http://localhost:8080/advertisement/list';
 
-            if (this.titleInputValue !== '') {
-                request += `${isHasParam === true ? '&' : '?'}title=${this.titleInputValue}`;
-                isHasParam = true;
+            if (isChangePage === true) {
+                this.filters = Object.assign({}, this.lastFilters);
             }
-            if (this.descriptionInputValue !== '') {
-                request += `${isHasParam === true ? '&' : '?'}description=${this.descriptionInputValue}`;
-                isHasParam = true;
+            else {
+                this.pageNumber = 1;
+            }
+
+            request += `?start_timestamp=${this.filters.startDate === null ? '2000-01-01' : this.filters.startDate} 00:00:00`;
+            request += `&end_timestamp=${this.filters.endDate === null ? '2099-12-31' : this.filters.endDate} 23:59:59`;
+
+            if (this.filters.title !== '') {
+                request += `&title=${this.filters.title}`;
+            }
+            if (this.filters.description !== '') {
+                request += `&description=${this.filters.description}`;
+            }
+            if (this.filters.category !== 'Все') {
+                request += `&category_id=${this.categories.indexOf(this.filters.category)}`;
             }
             if (this.pageSizeSelector !== 'Все') {
-                request += `${isHasParam === true ? '&' : '?'}page_size=${this.pageSizeSelector}`;
+                request += `&page_size=${this.pageSizeSelector}`;
                 request += `&page_number=${this.pageNumber}`;
-                isHasParam = true;
             }
-            console.log(request);
+            
             axios.get(request).then(response => {
+                console.log(request);
+                this.lastRequest = request;
+                this.lastFilters = Object.assign({}, this.filters);
+
                 if (this.pageSizeSelector === 'Все') {
                     this.advs = response.data;
-                    this.isPaging = false;
                 }
                 else {
                     this.advs = response.data.content;
                     this.totalPages = response.data.totalPages;
-                    this.isPaging = true;
                 }
-
-                console.log(this.totalPages);
             });
         },
-        toFirstPage: function() {
-            this.pageNumber = 1;
-            this.refreshList();
-        },
-        toPrevPage: function() {
-            this.pageNumber = this.pageNumber - 1;
-            if (this.pageNumber < 1) this.pageNumber = 1;
-            this.refreshList();
+        setPaging: function() {
+            if (this.pageSizeSelector === 'Все') {
+                this.isPaging = false;
+            }
+            else {
+                this.isPaging = true;
+            }
+            this.toPage(1);
         },
         toPage: function(i) {
+            if (i < 1) i = 1;
+            if (i > this.totalPages) i = this.totalPages;
             this.pageNumber = i;
-            this.refreshList();
-        },
-        toNextPage: function() {
-            this.pageNumber = this.pageNumber + 1;
-            if (this.pageNumber > this.totalPages) this.pageNumber = this.totalPages;
-            this.refreshList();
-        },
-        toLastPage: function() {
-            this.pageNumber = this.totalPages;
-            this.refreshList();
+            this.refreshList(true);
         },
         getAdvertisements: function() {
             axios.get('http://localhost:8080/advertisement/list').then(response => (this.advs = response.data));
@@ -134,6 +178,7 @@ export default {
         }
     },
     mounted: function() {
+        this.pageSizeSelector = 'Все';
         this.getAdvertisements();
     }
 }
@@ -153,7 +198,7 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: blue;
 }
 .page-selector a {
     margin: 0 0.25rem;
